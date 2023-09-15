1581ed86f2432ca69482aaf083582e84ea4ed783
burningcm <106593072+burningcm@users.noreply.github.com>
2023-09-15 20:03:00 +0900

    '2023. 9. 15. 오후 8:01:31 main.blocks, main.ts 업데이트하기' 복원하기

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showNumber(0)
-    }
-})
 basic.showString("123")
 basic.showString("456")
 basic.showString("Hello!")
